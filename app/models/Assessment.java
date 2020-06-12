6a0545618630f51e9dfbddf801811dd0072001d4
gsoady <gsoady19@gmail.com>
2020-06-12 11:31:36 +0100

    Added comment to member dashboard

--- app/models/Assessment.java
+++ app/models/Assessment.java
@@ -13,7 +13,6 @@ public class Assessment extends Model
   public double upperArm;
   public double waist;
   public double hips;
-
   public String comment;
 
   public Assessment(double weight, double chest, double thigh, double upperArm, double waist, double hips)
